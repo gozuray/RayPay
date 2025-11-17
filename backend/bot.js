f59ea5854dcf488b5fa38d386ebb024924038e35
gozuray <ray.peratta.e@gmail.com>
2025-11-17 00:19:01 +0000

    version v1

--- backend/bot.js
+++ backend/bot.js
@@ -37,11 +37,12 @@ function createSingleton() {
 
   async function ensureAuthFolder() {
   if (!authFolderPath) {
-   authFolderPath = "/opt/data/whatsapp-session";
+    authFolderPath = "/tmp/whatsapp-session";
     await fs.promises.mkdir(authFolderPath, { recursive: true });
   }
   return authFolderPath;
 }
+
 
 
   class MongoSessionStore {
